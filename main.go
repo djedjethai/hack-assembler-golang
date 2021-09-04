54c4aa24a85f5c11c300209067e1c719c7d75c4d
Jerome Bidault <djedje.thai.ok@gmail.com>
2021-09-04 21:08:24 +0700

    assemble MaxL.asm works fine

--- main.go
+++ main.go
@@ -12,22 +12,20 @@ import (
 )
 
 var (
-	r = regexp.MustCompile(`^(.{1}).*`)
-	i = regexp.MustCompile(`^(.*)=|;(.*)\s$`)
-	s = regexp.MustCompile(`^.*(;).*$`)
+	r        = regexp.MustCompile(`^(.{1}).*`)
+	i        = regexp.MustCompile(`^(.*)=(.*)\s$`)
+	ii       = regexp.MustCompile(`^(.*);(.*)\s$`)
+	rj       = regexp.MustCompile(`^.*(;).*$`)
+	rd       = regexp.MustCompile(`^.*(=).*$`)
+	binary   strings.Builder
+	sb       strings.Builder
+	codeStrr string
 )
 
-// A instruction: @value
-// 0 vvv vvvv vvvv vvvv
-
-// C instruction
-//  1 1 1 c0(a) c1 c2 c3 c4 c5 c6 d1 d2 d3 j1 j2 j3
-// dest = comp; jump
-
 func main() {
-	// cInstruction := tableParser("./tables/cInstructions.txt")
-	// cDestination := tableParser("./tables/cInstDestination.txt")
-	// cJump := tableParser("./tables/cJump.txt")
+	cInstruction := tableParser("./tables/cInstructions.txt")
+	cDestination := tableParser("./tables/cInstDestination.txt")
+	cJump := tableParser("./tables/cJump.txt")
 	// fmt.Println(cJump)
 
 	// parse progTest
@@ -36,48 +34,29 @@ func main() {
 	progData, _ := ioutil.ReadFile(prog)
 	progText := string(progData)
 
-	var binary strings.Builder
-	// var sb strings.Builder
-	var ref string // ref to previous ranged line
 	for _, line := range strings.Split(progText, "\n") {
 		if len(line) > 0 {
 			firstChar := r.FindAllStringSubmatch(line, -1)[0][1]
 
-			// fmt.Printf("grrrrrr: %v\n", s.MatchString(line))
-			// fmt.Println("the firssst: ", firstChar)
-
 			if firstChar == "@" {
-				ref = line
-			} else if ref != "" &&
-				s.MatchString(line) {
-				fmt.Println("In a: ", line)
-				ref = ""
-			} else if ref != "" {
-				fmt.Println("In b: ", line)
-				ref = ""
+				codeStrr = assembleAddress(line, sb)
+				binary.WriteString(codeStrr)
+
+			} else if rj.MatchString(line) {
+				codeStrr = assembleJump(line, sb, cInstruction, cJump)
+				binary.WriteString(codeStrr)
+
+			} else if rd.MatchString(line) {
+				codeStrr = assembleNoJump(line, sb, cInstruction, cDestination)
+				binary.WriteString(codeStrr)
+
 			} else {
-				// fmt.Println("other cases: ", line)
+				fmt.Println("other cases: ", line)
 			}
-			// if line start @ save it
-			// break
-
-			// if saved line and the current line is ;
-			// consume the saved line(as jump) with the current one
-			// empty saved line
-			// break
-
-			// else if saved line assembleNoJump saved line and curent one
-			// empty saved line
-			// break
-
-			// else cunsume current assembleNoJump
-
-			// codeStrr := assembleNoJump(r, line, sb, cInstruction, cDestination)
-			// binary.WriteString(codeStrr)
 		}
 	}
 	fmt.Println("Binaries")
-	codeStrr := binary.String()
+	codeStrr = binary.String()
 	fmt.Println(codeStrr)
 	codeBin := []byte(codeStrr)
 	if err := ioutil.WriteFile("Add.hack", codeBin, 0777); err != nil {
@@ -85,38 +64,48 @@ func main() {
 	}
 }
 
-func assembleNoJump(r *regexp.Regexp, line string, sb strings.Builder, cInstruction map[string]string, cDestination map[string]string) string {
-	firstChar := r.FindAllStringSubmatch(line, -1)[0][1]
-	// will need to add case "(" and "0" later on
-	switch firstChar {
-	case "@":
-		i, err := strconv.Atoi(strings.TrimSpace(line[1:]))
-		if err != nil {
-			fmt.Println(err)
-		}
-		b := completeBitsFront(strconv.FormatInt(int64(i), 2), 16)
-		// fmt.Println(reflect.TypeOf(b))
-		sb.WriteString(b)
-		sb.WriteString("\n")
-
-		return sb.String()
-	case "M", "A", "D":
-		sb.WriteString("111")
-
-		// same block(no jump)
-		c := i.FindAllStringSubmatch(line, -1)[0][2]
-		sb.WriteString(string(cInstruction[c]))
-
-		d := i.FindAllStringSubmatch(line, -1)[0][1]
-		sb.WriteString(string(cDestination[d]))
-
-		final := completeBitsBack(sb.String(), 16)
-
-		return final
-	default:
-		fmt.Println("empty or comm: ", line)
-		return ""
+func assembleAddress(line string, sb strings.Builder) string {
+	i, err := strconv.Atoi(strings.TrimSpace(line[1:]))
+	if err != nil {
+		fmt.Println(err)
 	}
+	b := completeBitsFront(strconv.FormatInt(int64(i), 2), 16)
+	// fmt.Println(reflect.TypeOf(b))
+	sb.WriteString(b)
+	sb.WriteString("\n")
+
+	return sb.String()
+}
+
+func assembleJump(line string, sb strings.Builder, cInstruction map[string]string, cJump map[string]string) string {
+	sb.WriteString("111")
+
+	c := ii.FindAllStringSubmatch(line, -1)[0][1]
+
+	sb.WriteString(string(cInstruction[c]))
+
+	sb.WriteString(string("000"))
+
+	j := ii.FindAllStringSubmatch(line, -1)[0][2]
+
+	sb.WriteString(string(cJump[j]))
+	sb.WriteString("\n")
+
+	return sb.String()
+}
+
+func assembleNoJump(line string, sb strings.Builder, cInstruction map[string]string, cDestination map[string]string) string {
+	sb.WriteString("111")
+
+	c := i.FindAllStringSubmatch(line, -1)[0][2]
+	sb.WriteString(string(cInstruction[c]))
+
+	d := i.FindAllStringSubmatch(line, -1)[0][1]
+	sb.WriteString(string(cDestination[d]))
+
+	final := completeBitsBack(sb.String(), 16)
+
+	return final
 }
 
 func completeBitsFront(s string, lgt int) string {
@@ -161,6 +150,78 @@ func tableParser(filename string) map[string]string {
 	}
 	return cData
 }
+
+// =============================================================
+
+// func assembleJump(r *regexp.Regexp, line string, sb strings.Builder, cInstruction map[string]string, cJump map[string]string) string {
+// 	firstChar := r.FindAllStringSubmatch(line, -1)[0][1]
+// 	switch firstChar {
+// 	case "@":
+// 		// fmt.Println("ahhh: ", line)
+// 		i, err := strconv.Atoi(strings.TrimSpace(line[1:]))
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		b := completeBitsFront(strconv.FormatInt(int64(i), 2), 16)
+// 		// fmt.Println(reflect.TypeOf(b))
+// 		sb.WriteString(b)
+// 		sb.WriteString("\n")
+//
+// 		return sb.String()
+// 	case "M", "A", "D", "0":
+// 		sb.WriteString("111")
+//
+// 		// same block(no jump)
+// 		c := i.FindAllStringSubmatch(line, -1)[0][1]
+// 		sb.WriteString(string(cInstruction[c]))
+// 		sb.WriteString(string("000"))
+// 		j := i.FindAllStringSubmatch(line, -1)[0][2]
+// 		sb.WriteString(string(cJump[j]))
+//
+// 		return sb.String()
+// 	default:
+// 		// fmt.Println("empty or comm: ", line)
+// 		return ""
+// 	}
+//
+// }
+//
+// func assembleNoJump(r *regexp.Regexp, line string, sb strings.Builder, cInstruction map[string]string, cDestination map[string]string) string {
+// 	firstChar := r.FindAllStringSubmatch(line, -1)[0][1]
+// 	// will need to add case "(" and "0" later on
+// 	switch firstChar {
+// 	case "@":
+// 		// fmt.Println("ahhh: ", line)
+// 		i, err := strconv.Atoi(strings.TrimSpace(line[1:]))
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		b := completeBitsFront(strconv.FormatInt(int64(i), 2), 16)
+// 		// fmt.Println(reflect.TypeOf(b))
+// 		sb.WriteString(b)
+// 		sb.WriteString("\n")
+//
+// 		return sb.String()
+// 	case "M", "A", "D":
+// 		sb.WriteString("111")
+//
+// 		// same block(no jump)
+// 		c := i.FindAllStringSubmatch(line, -1)[0][2]
+// 		sb.WriteString(string(cInstruction[c]))
+//
+// 		d := i.FindAllStringSubmatch(line, -1)[0][1]
+// 		sb.WriteString(string(cDestination[d]))
+//
+// 		final := completeBitsBack(sb.String(), 16)
+//
+// 		return final
+// 	default:
+// 		// fmt.Println("empty or comm: ", line)
+// 		return ""
+// 	}
+// }
+
+// =========================================================
 
 // package main
 //

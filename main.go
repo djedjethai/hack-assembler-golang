b61d6195c904062b028dbdb503b55fd4fad59e28
Jerome Bidault <djedje.thai.ok@gmail.com>
2021-09-20 17:02:25 +0700

    refactor all func done

--- main.go
+++ main.go
@@ -5,31 +5,17 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"path/filepath"
-	// "reflect"
 	"log"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
 var (
-	// first parsing regexp
-	rj         = regexp.MustCompile(`^\s*?(.{1,3});([a-zA-Z]{1,4}).*$`)
-	rd         = regexp.MustCompile(`^\s*?([a-zA-Z]{1,4})=(.{1,3}).*$`)
-	binary     strings.Builder
-	sb         strings.Builder
-	dv         = regexp.MustCompile(`^\s*?\((.*)\).*$`)
-	vv         = regexp.MustCompile(`^\s*?@([^SP|LCL|ARG|THAT|THIS][A-Za-z.]+[^$_0-9])*$`)
-	vvcomplete = regexp.MustCompile(`^.*@([A-Za-z.]+\.[0-9]).*$`)
-	le         = regexp.MustCompile(`^\s*?(\/\/.*|\s*)$`)
-
-	// second parsing regexp
-	rv    = regexp.MustCompile(`^\s*?@R([0-9]{1,2}).*$`)
-	tthat = regexp.MustCompile(`^\s*?@(SP|LCL|ARG|THAT|THIS|ponggame.0|math.1|math.0|memory.0|output.6|output.5|output.4|output.3|output.2|output.1|output.0|screen.1|screen.2|screen.0).*$`)
-	gv    = regexp.MustCompile(`^\s*?@(\D[A-Za-z._$]*[0-9]*).*`)
-	ga    = regexp.MustCompile(`^\s*?@(\d*).*`)
-
+	rj           = regexp.MustCompile(`^\s*?(.{1,3});([a-zA-Z]{1,4}).*$`)
+	rd           = regexp.MustCompile(`^\s*?([a-zA-Z]{1,4})=(.{1,3}).*$`)
+	binary, sb   strings.Builder
 	varVar           = make(map[string]string)
 	countDestVar int = 0 // start at 0
 	countVarVar  int = 0 // start at 0
@@ -57,7 +43,7 @@ func main() {
 	// second parsing set the binary file
 	secondParseSetBinaries(progText)
 
-	fmt.Println("Binaries")
+	fmt.Println("Binaries ready")
 	codeStrr := binary.String()
 	codeBin := []byte(codeStrr)
 	if err := ioutil.WriteFile("Add.hack", codeBin, 0777); err != nil {
@@ -66,6 +52,11 @@ func main() {
 }
 
 func firstParseSetVariousVar(progText string) {
+	dv := regexp.MustCompile(`^\s*?\((.*)\).*$`)
+	vv := regexp.MustCompile(`^\s*?@([^SP|LCL|ARG|THAT|THIS][A-Za-z.]+[^$_0-9])*$`)
+	vvcomplete := regexp.MustCompile(`^.*@([A-Za-z.]+\.[0-9]).*$`)
+	le := regexp.MustCompile(`^\s*?(\/\/.*|\s*)$`)
+
 	for _, line := range strings.Split(progText, "\n") {
 
 		if len(line) > 0 {
@@ -106,6 +97,11 @@ func secondParseSetBinaries(progText string) {
 	cJump := tableParser("./tables/cJump.txt")
 	rVar := tableParser("./tables/rVariable.txt")
 	thisThat := tableParser("./tables/thisThatTable.txt")
+
+	rv := regexp.MustCompile(`^\s*?@R([0-9]{1,2}).*$`)
+	tthat := regexp.MustCompile(`^\s*?@(SP|LCL|ARG|THAT|THIS|ponggame.0|math.1|math.0|memory.0|output.6|output.5|output.4|output.3|output.2|output.1|output.0|screen.1|screen.2|screen.0).*$`)
+	gv := regexp.MustCompile(`^\s*?@(\D[A-Za-z._$]*[0-9]*).*`)
+	ga := regexp.MustCompile(`^\s*?@(\d*).*`)
 
 	for _, line := range strings.Split(progText, "\n") {
 		var v string
